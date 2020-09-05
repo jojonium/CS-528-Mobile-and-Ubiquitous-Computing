3fb9e44cdc2d02e7d171b1d09d421ba5f80f9170
Joseph Petitti <jppetitti@wpi.edu>
2020-09-04 21:19:56 -0400

    add project0 skeleton

---
+++ project0/app/src/main/java/org/petitti/project0/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.petitti.project0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project0.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
