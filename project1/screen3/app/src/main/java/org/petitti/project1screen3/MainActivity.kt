213925200fbba3d1500f3ee6a1b1e758a6391ef9
Joseph Petitti <jppetitti@wpi.edu>
2020-09-15 01:43:42 -0400

    finish screen 3

--- project1/screen3/app/src/main/java/org/petitti/project1screen3/MainActivity.kt
+++ project1/screen3/app/src/main/java/org/petitti/project1screen3/MainActivity.kt
@@ -2,10 +2,27 @@ package org.petitti.project1screen3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    var webView: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.webView = findViewById<WebView>(R.id.webView)
+    }
+
+    fun onClickGoogle(view: View) {
+        webView?.loadUrl("https://www.google.com/")
+    }
+
+    fun onClickWikipedia(view: View) {
+        webView?.loadUrl("https://www.wikipedia.org/")
+    }
+
+    fun onClickJosephPetitti(view: View) {
+        webView?.loadUrl("https://josephpetitti.com/")
     }
 }
